15e4d92a95f40fbd60397af93bbad027159ee4e6
taingy-srun <taingy.srun20@gmail.com>
2024-04-04 14:57:56 -0500

    using modelmap

--- src/main/java/edu/miu/cs489/taingy/elibrary/controller/ELibraryController.java
+++ src/main/java/edu/miu/cs489/taingy/elibrary/controller/ELibraryController.java
@@ -1,13 +1,15 @@
 package edu.miu.cs489.taingy.elibrary.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ELibraryController {
 
     @GetMapping(value = {"/", "/elibrary", "/elibrary/home"})
-    public String home() {
+    public String home(ModelMap model) {
+        model.addAttribute("name", "Taingy");
         return "/home/index.html";
     }
 }
